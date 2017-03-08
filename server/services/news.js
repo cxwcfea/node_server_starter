abcca7f9a9ffa218f13829ad28884cb1e75c1a02
cxwcfea <cxwcfea@163.com>
2017-03-08 23:30:14 +0800

    use Promise.all instead of axios.all

--- server/services/news.js
+++ server/services/news.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import axios from 'axios';
+import Promise from 'bluebird';
 
 const SERVER_URL = 'https://hacker-news.firebaseio.com/v0';
 const PAGE_SIZE = 15;
@@ -15,7 +16,7 @@ export default {
     });
 
     const urlList = _.map(idList, _.identity).map(elem => axios(`${SERVER_URL}/item/${elem}.json`));
-    const newsList = await axios.all(urlList);
+    const newsList = await Promise.all(urlList);
     return newsList.map(elem => elem.data);
   },
 };
